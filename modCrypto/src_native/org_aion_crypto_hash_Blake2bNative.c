067a5dbb3377828f396d95b7b20d429136b0b41b
kelvin <kelvinlam@kelvinlam>
2018-08-27 12:11:57 -0400

    osx compilation support for blake2b

--- modCrypto/src_native/org_aion_crypto_hash_Blake2bNative.c
+++ modCrypto/src_native/org_aion_crypto_hash_Blake2bNative.c
@@ -24,15 +24,33 @@
 #include "org_aion_crypto_hash_Blake2bNative.h"
 #include "blake2.h"
 #include <string.h> // for functions memset
-#include <endian.h>
 #include <stdint.h> // for types uint32_t,uint64_t
 
+#ifdef __APPLE__
+#include <machine/endian.h>
+#include <libkern/OSByteOrder.h>
+#define htobe16(x) OSSwapHostToBigInt16(x)
+#define htole16(x) OSSwapHostToLittleInt16(x)
+#define be16toh(x) OSSwapBigToHostInt16(x)
+#define le16toh(x) OSSwapLittleToHostInt16(x)
+#define htobe32(x) OSSwapHostToBigInt32(x)
+#define htole32(x) OSSwapHostToLittleInt32(x)
+#define be32toh(x) OSSwapBigToHostInt32(x)
+#define le32toh(x) OSSwapLittleToHostInt32(x)
+#define htobe64(x) OSSwapHostToBigInt64(x)
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define be64toh(x) OSSwapBigToHostInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
+#else
+#include <machine/endian.h>
+#endif 
 
 #define HASH_LEN 32
 #define HASHESPERBLAKE 2
 #define HASHOUT 54
 #define N 210
 #define K 9
+
 
 typedef uint32_t u32;
 typedef unsigned char uchar;
